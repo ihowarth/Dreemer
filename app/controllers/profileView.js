6625994f94aa901665259edebf625fef104a62c6
Ian Howarth <idhowarth7@gmail.com>
2015-03-29 18:00:53 -0600

    working on 'My Profile' UI

--- app/controllers/profileView.js
+++ app/controllers/profileView.js
@@ -20,6 +20,16 @@ function addEventListeners() {
 	$.backButton.addEventListener( "click" , function() {
 		$.containerView.animate({ opacity : 0 , duration : 600 }, args.closeProfileView );
 	});
+	$.dreemsControlTabbedBar.addEventListener( "click" , function( e ) {
+	    //TODO: Handle showing different tables and changing no results label text.
+	    // Index 0 is for 'My Dreems' and 1 is for 'Endorsed'
+	    if ( e.index == 0 ) {
+	        
+	        
+	    } else {
+	        
+	    }
+	});
 }
 
 
